d832886ddc1d47716742f29d95ac79e2f1010459
Sebx-code <steevebille1@gmail.com>
2025-10-29 11:11:46 +0100

    first commit

---
+++ JavaFXApp/Main.java
@@ -0,0 +1,22 @@
+package JavaFXApp.JavaFXApp;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Bienvenue dans JavaFX sur VS Code !");
+        Scene scene = new Scene(label, 400, 200);
+        stage.setTitle("JavaFX Demo");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
